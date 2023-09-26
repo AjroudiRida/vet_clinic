32d69eaae2b2fa546798f60e5d58b5c6e66801a1
ajroudirida <hpwin10kamilio@gmail.com>
2023-09-26 17:32:13 +0100

    Vet clinic database: query and update animals table

--- data.sql
+++ data.sql
@@ -4,4 +4,11 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2,'Gabumon',TO_DATE('Nov 15, 2018', 'Mon DD, YYYY'),2,true,8);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Pikachu',TO_DATE('Jan 7, 2021', 'Mon DD, YYYY'),1,false,15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4,'Devimon',TO_DATE('May 12, 2017', 'Mon DD, YYYY'),5,true,11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (1,'Charmander',TO_DATE('Feb 8, 2020', 'Mon DD, YYYY'),0,false,-11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2,'Plantmon',TO_DATE('Nov 15, 2021', 'Mon DD, YYYY'),2,true,-5.7);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Squirtle',TO_DATE('Apr 2, 1993', 'Mon DD, YYYY'),3,false,-12.13);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4,'Angemon',TO_DATE('Jun 12, 2005', 'Mon DD, YYYY'),1,true,-45);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (5,'Boarmon',TO_DATE('Jun 7, 2005', 'Mon DD, YYYY'),7,true,20.4);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (6,'Blossom',TO_DATE('Oct 13, 1998', 'Mon DD, YYYY'),3,true,17);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (7,'Ditto',TO_DATE('May 14, 2022', 'Mon DD, YYYY'),4,true,22);
 
--- schema.sql
+++ schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE animals (
     escape_attempts integer,
     neutered boolean,
     weight_kg decimal,
+    species text,
     PRIMARY KEY (id)
 );
